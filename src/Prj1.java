883f880a2c8ba69ff6a297b49ec8d54dc3a8d6ce
SwapnaliJ <swapnalij4050@gmail.com>
2020-12-03 15:30:15 +0530

    firstComit

---
+++ src/Prj1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Prj1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in= new Scanner(System.in);
+		int j=in.nextInt();
+		
+	if(j>5)
+	
+	{
+		System.out.println("Hello");
+	}
+	else System.out.println("Bye");
+	}
+
+}
